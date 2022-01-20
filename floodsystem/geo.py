b824630f07d39bb8853a7de599d275608ceef5af
k12ish <k4krish@gmail.com>
2022-01-20 10:53:05 +0000

    Added Docstrings

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -22,6 +22,9 @@ def stations_by_distance(stations, p):
 
 
 def stations_within_radius(stations, centre, r):
+    """
+    returns a list of all stations (type MonitoringStation) within radius r of centre.
+    """
     stations_by_dist = stations_by_distance(stations, centre)
     stations = [s for (s, _) in stations_by_dist]
     distances = [d for (_, d) in stations_by_dist]
@@ -50,6 +53,10 @@ def stations_by_river(stations):
 
 
 def rivers_by_station_number(stations, N):
+    """
+    determines the N rivers with the greatest number of monitoring stations.
+    returns a list of (river name, number of stations) tuples
+    """
     count = Counter(s.river for s in stations)
     count_of_nth = count.most_common(N)[-1][1]
     return [(r, c) for (r, c) in count.most_common() if c >= count_of_nth]
