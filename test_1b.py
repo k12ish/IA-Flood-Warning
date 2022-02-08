ce7ebf42a1530a6db8985874ab78cd290b8d9a35
piersbow <piers@disroot.org>
2022-02-08 09:07:56 +0000

    Added test for 1b

---
+++ test_1b.py
@@ -0,0 +1,22 @@
+from floodsystem.geo import stations_by_distance
+from floodsystem.station import MonitoringStation
+import haversine
+
+
+def test_1b():
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord1 = (1, 1)
+    trange = None
+    river = "My River"
+    town = "My Town"
+    s1 = MonitoringStation(s_id, m_id, label, coord1, trange, river, town)
+    coord2 = (1000, 1000)
+    s2 = MonitoringStation(s_id, m_id, label, coord2, trange, river, town)
+    stations = [s1, s2]
+    sorted_stations = stations_by_distance(stations, (0,0))
+
+    assert sorted_stations[0][0] is s1
+
+    assert sorted_stations[0][1] == haversine.haversine((0,0), coord1)
