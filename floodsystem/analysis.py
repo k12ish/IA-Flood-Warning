db320890386966339a2e5bf4c05e3ab4bc91943e
k12ish <k4krish@gmail.com>
2022-02-27 15:20:04 +0000

    code to predict future date

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -16,3 +16,21 @@ def polyfit(dates, levels, p):
     coeff = np.polyfit(x - d0, levels, p)
     poly = np.poly1d(coeff)
     return poly, d0
+
+
+def predict_future_level(dates, levels):
+    """
+    given the water level time history (dates, levels) for a station,
+    return tuple of future date, water level at thatdate
+    """
+    assert len(dates) == len(levels)
+    assert len(dates) > 0
+
+    x = matplotlib.dates.date2num(dates)
+    d0 = sum(x) / len(x)
+    coeff = np.polyfit(x - d0, levels, 3)
+    poly = np.poly1d(coeff)
+
+    # one day after the most recent day
+    future_date = max(x) + 1
+    return matplotlib.num2date(future_date), poly(future_date - d0)
