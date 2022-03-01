8f2ed0aaf51903a6a30d41456a3c45dfb9de566b
k12ish <k4krish@gmail.com>
2022-03-01 09:39:38 +0000

    Display results incrementally

--- Task2G.py
+++ Task2G.py
@@ -7,21 +7,19 @@ from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.analysis import predict_future_level
 from floodsystem.datafetcher import fetch_measure_levels
 
-
 stations = build_station_list()
 update_water_levels(stations)
 
-risk_levels = []
 
-for station in stations:
-
+def risk_level(station):
     try:
         dates, levels = fetch_measure_levels(
-            station.measure_id, dt=datetime.timedelta(days=10)
+            station.measure_id, dt=datetime.timedelta(days=3)
         )
         level_tommorow = predict_future_level(dates, levels)[1]
         realtive_level_tommorow = (level_tommorow - station.typical_range[0]) / (
-                station.typical_range[1] - station.typical_range[0])
+            station.typical_range[1] - station.typical_range[0]
+        )
     except:
         print("something happened")
         realtive_level_tommorow = 1
@@ -30,18 +28,20 @@ for station in stations:
 
     # if already flooded, eg water level more the double typical, rank severe
     if current_water_level > 2:
-        risk_levels.append([station, "Severe"])
+        return "Severe"
     # if extraperlated will flood within time X make severe
     elif realtive_level_tommorow > 1.9:
-        risk_levels.append([station, "Severe"])
+        return "Severe"
     # if extrapolated will flood within time Y make high
     elif realtive_level_tommorow > 1.5:
-        risk_levels.append([station, "High"])
+        return "High"
     elif current_water_level > 1.6:
-        risk_levels.append([station, "High"])
+        return "High"
     elif current_water_level > 1.4:
-        risk_levels.append([station, "Moderate"])
+        return "Moderate"
     else:
-        risk_levels.append([station, "Low"])
+        return "Low"
 
-print(risk_levels)
+
+for station in stations:
+    print("Risk Level for", station.name, "is", risk_level(station))
